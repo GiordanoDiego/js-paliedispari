60e2a11890ccc3ed65b3b02c04829f95b513c36b
GiordanoDiego <diegomgiordano96@gmail.com>
2023-12-12 15:35:05 +0100

    start

---
+++ dispari/js/script.js
@@ -0,0 +1,22 @@
+/*
+console.log('nomeVariabile', nomeVariabile, typeof nomeVariabile);
+
+document.getElementById('idHtml').innerHTML = noveVariabileJS;
+
+//valore input 
+    userName = document.getElementById('userName');
+    userNameValue = userName.value;
+
+//controllare se il valore è Nan
+    if(isNaN(kmValue))
+
+*/
+
+/*
+    Pari e Dispari
+    L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+    Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    Sommiamo i due numeri
+    Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+    Dichiariamo chi ha vinto.
+*/
